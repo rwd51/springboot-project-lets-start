d9d30239eebb11ce388dedb9d29e3cbf0a0ceb82
Ruwad Naswan <2105051@ugrad.cse.buet.ac.bd>
2025-08-31 02:22:51 +0600

    lets start

---
+++ src/main/java/com/mlvu/mlvu_auth/MlvuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mlvu.mlvu_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlvuAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlvuAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlvu/mlvu_auth/MlvuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlvu.mlvu_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlvuAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
